ea62b56fe85ff4b3394dc04ec20a00637fa3fdd7
Jordyn <jordyn@husk.pro>
2020-08-14 16:18:28 +0800

    begin frontend work

---
+++ static/js/vueApp.js
@@ -0,0 +1,34 @@
+Vue.component('project', {
+    props: {
+        projectName: String,
+        buildStatus: String
+    },
+    template: '<div class="col-lg-3 col-md-6 text-center">\n' +
+        '                        <div class="mt-5">\n' +
+        '                            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>\n' +
+        '                            <h3 class="h4 mb-2">{{ projectName }}</h3>\n' +
+        '                            <img :src="buildStatus" alt="Build status badge"/>\n' +
+        '                        </div>\n' +
+        '                    </div>'
+})
+
+new Vue({
+    el: '#projects',
+    data: {
+        loading: true,
+        projects: []
+    },
+
+    mounted() {
+        axios
+            .get('/api/projects')
+            .then(response => (this.projects = response.data.projects))
+            .finally(() => this.loading = false)
+    },
+
+    methods: {
+        badgeUrlForProject: function (projectName) {
+            return "/" + projectName + "/badge";
+        }
+    },
+})
