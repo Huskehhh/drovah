6583e4267c74804feaf97679c55bfbb863e27bed
Jordyn <jordyn@husk.pro>
2020-08-28 12:20:30 +0800

    drop the build component for now

--- static/js/vueApp.js
+++ static/js/vueApp.js
@@ -20,23 +20,8 @@ Vue.component('project', {
     template: '<div class="col-lg-3 col-md-6 text-center">\n' +
         '                        <div class="mt-5" style="padding-bottom: 30%;">\n' +
         '                            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>\n' +
-        '                            <p class="p mb-2">{{ projectName }} <a v-if="hasLatest()" v-bind:href="latestBuildForProject()"><i class="fas fa-download"></i></a></p>\n' +
+        '                            <p class="p mb-2">{{ projectName }} <a v-if="hasLatest()" v-bind:href="latestBuild()"><i class="fas fa-download"></i></a></p>\n' +
         '                            <img v-bind:src="badgeUrl()" alt="Build status badge"/>\n' +
-        '                        </div>\n' +
-        '                    </div>'
-})
-
-Vue.component('build', {
-    props: {
-        buildNumber: Number,
-        buildStatus: String,
-        archivedFiles: Array,
-    },
-    template: '<div class="col-lg-3 col-md-6 text-center">\n' +
-        '                        <div class="mt-5" style="padding-bottom: 30%;">\n' +
-        '                            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>\n' +
-        '                            <p class="p mb-2">{{ projectName }} <a v-if="hasFiles" v-bind:href="latestBuild"><i class="fas fa-download"></i></a></p>\n' +
-        '                            <img v-bind:src="buildStatus" alt="Build status badge"/>\n' +
         '                        </div>\n' +
         '                    </div>'
 })
